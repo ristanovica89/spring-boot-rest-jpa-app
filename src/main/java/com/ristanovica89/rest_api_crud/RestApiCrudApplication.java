edf6cca6acdfd684e77fdee0a5bc80636ec30bd2
ristanovica89 <ristanovica89@gmail.com>
2025-09-27 22:23:34 +0200

    Initial commit.

---
+++ src/main/java/com/ristanovica89/rest_api_crud/RestApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ristanovica89.rest_api_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ristanovica89/rest_api_crud/RestApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ristanovica89.rest_api_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
